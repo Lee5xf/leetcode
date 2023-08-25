ca2e871e472e3e58657269d45739ad04a5bf13c9
Lee <820501696@qq.com>
2023-08-25 22:08:56 +0800

    day3 206.反转链表

---
+++ 206.反转链表.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=javascript
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+    const node = new ListNode(0, null);
+    let p = head
+    while (p) {
+        let r = p.next
+        p.next = node.next
+        node.next = p
+        p = r
+    }
+    return node.next
+};
+// @lc code=end
+
